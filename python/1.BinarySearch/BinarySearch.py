09557241e37fafd998c883eafbbdf889e98ecf98
flyleaf91 <flyleaf91@163.com>
2016-03-07 20:22:40 +0800

    binarysearch python

---
+++ python/1.BinarySearch/BinarySearch.py
@@ -0,0 +1,17 @@
+#!usr/bin/python#coding=utf-8
+#二分查找
+#执行方式: python BinarySearch.py 白名单 数据
+
+import sys
+
+def gcd(q,p):
+
+    r=q%p 
+    if r==0:
+        return p
+    else:       
+        return gcd(q,r)    
+        
+
+t = gcd(int(sys.argv[1]),int(sys.argv[2]))
+print(t)    
